23308969721fac73da903645162227eed7b61f35
JaneIndy <95003003+JaneIndy@users.noreply.github.com>
2022-04-06 18:53:45 -0400

    janeney update

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -1,12 +1,15 @@
 package driver;
 
 import pages.CommonPage;
+import pages.CommonPage_BAU_TC305;
 
 public class PageInitializer extends BaseClass {
 
 	public static CommonPage cP;
+	public static CommonPage_BAU_TC305 Cpb;
 
 	public static void initialize() {
 		cP = new CommonPage();
+		Cpb= new CommonPage_BAU_TC305();
 	}
 }
