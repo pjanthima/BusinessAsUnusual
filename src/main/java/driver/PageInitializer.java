ffebfbe491de9fe73fea7a40ae5b97524cc3f923
Wthongkom <thongkom.w@gmail.com>
2022-04-07 01:34:42 -0400

    Summit BAU_TC205 By Watcharapong

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -1,12 +1,15 @@
 package driver;
 
 import pages.CommonPage;
+import pages.CommonPage_BAU_TC205;
 
 public class PageInitializer extends BaseClass {
 
 	public static CommonPage cP;
+	public static CommonPage_BAU_TC205 cPTC205;
 
 	public static void initialize() {
 		cP = new CommonPage();
+		cPTC205 = new CommonPage_BAU_TC205();
 	}
 }
