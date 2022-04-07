082bd16dcfde5b8faa188d2d8018cdf7545fe411
kalthera <kalthera.chuinklin@gmail.com>
2022-04-06 22:23:05 -0400

    Done

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -1,12 +1,20 @@
 package driver;
 
 import pages.CommonPage;
+import pages.CommonPageTaylor;
+import pages.ContactUsPage;
 
 public class PageInitializer extends BaseClass {
 
 	public static CommonPage cP;
+	
+	public static CommonPageTaylor cPT;
+	public static ContactUsPage cU;
+
 
 	public static void initialize() {
 		cP = new CommonPage();
+		cPT = new CommonPageTaylor();
+		cU = new ContactUsPage();
 	}
 }
