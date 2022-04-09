52cb6145c74019ce7b017ed5f4ab9e991340ec1a
Brekhna Tufail <95002981+BrekhnaTufail@users.noreply.github.com>
2022-04-08 22:36:48 -0400

    Brekhna update
    can someone look over this

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -1,12 +1,19 @@
 package driver;
 
+import org.openqa.selenium.interactions.Actions;
+
 import pages.CommonPage;
+import pages.CommonPage_BAU_201;
 
 public class PageInitializer extends BaseClass {
 
 	public static CommonPage cP;
-
+     
+	public static CommonPage_BAU_201 cP201;
+	
 	public static void initialize() {
 		cP = new CommonPage();
+		cP201 = new CommonPage_BAU_201();
+		
 	}
 }
--- src/test/java/shopWebPage/BAU_TC201.java
+++ src/test/java/shopWebPage/BAU_TC201.java
@@ -1,7 +1,13 @@
 package shopWebPage;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -17,10 +23,56 @@ public class BAU_TC201 extends CommonMethods {
 		BaseClass.getDriver().manage().window().maximize();
 	}
 
-	@Test
-	public void Test() throws InterruptedException {
-		cP.closePopup.click();
-
+	@Test(enabled = true)
+	public void Test01() {
+		cP201.closePopup.click();
+		cP201.seacrchBox.sendKeys("iphone case");
+		cP201.magnifyingGlass.click();
+		scrollDown(500);
+		jsClick(cP201.colorBlack);
+		jsClick(cP201.iphone6Button);
+		
+		
 	}
 
+	@Test(enabled = true)
+	public void Test02() {
+		cP201.closePopup.click();
+		cP201.seacrchBox.sendKeys("shaker bottle");
+		cP201.magnifyingGlass.click();
+		scrollDown(1500);
+		jsClick(cP201.proSeriesButton);		
+  		waitForClickability(cP201.seeMoreButton);
+		jsClick(cP201.seeMoreButton);
+		TakesScreenshot("Test02.//ScreenShot/screen.png");
+		scrollDown(1500);
+		cP201.letterP.click();
+		cP201.proSeriesButton.click();
+		
+	}
+
+	@Test(enabled = true)
+	public void Test03() {
+		cP201.closePopup.click();
+		cP201.seacrchBox.sendKeys("desk");
+		waitForClickability(cP201.PrepacComputerDesk);
+		jsClick(cP201.PrepacComputerDesk);
+		TakesScreenshot("Test04.//ScreenShot/screen.png");
+		
+	}
+
+	@Test(enabled = true)
+	public void Test04() {
+		cP201.closePopup.click();
+		cP201.seacrchBox.sendKeys("beach ball");
+		cP201.magnifyingGlass.click();
+		waitForClickability(cP201.seeMoreButton_04);
+		jsClick(cP201.seeMoreButton_04);
+		TakesScreenshot("Test04.//ScreenShot/screen.png");
+		jsClick(cP201.disneyButton);
+		cP201.addToCartButton.click();
+		cP201.viewCart_Checkout.click();
+		
+
+	}
 }
