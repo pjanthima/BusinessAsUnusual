f5bae373f305cd8be88bd0c520890f8f6799a222
Phanuwat Janthima <phanuwatjanthima@Phanuwats-MBP.fios-router.home>
2022-04-09 02:48:31 -0400

    Update

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -2,14 +2,42 @@ package driver;
 
 import pages.CommonPage;
 import pages.CommonPage_BAU_TC204;
+import pages.CommonPageTaylor;
+import pages.ContactUsPage;
+import pages.CommonpageBAU_TC302;
+import pages.CommonPage_BAU_TC207;
+import pages.CommonPage_BAU_TC305;
 
 public class PageInitializer extends BaseClass {
 
 	public static CommonPage cP;
 	public static CommonPage_BAU_TC204 cP204;
 
+	public static CommonPageTaylor cPT;
+
+	public static ContactUsPage cU;
+
+	public static CommonpageBAU_TC302 cPG;
+
+	public static CommonPage_BAU_TC207 case1;
+
+	public static CommonPage_BAU_TC305 Cpb;
+
 	public static void initialize() {
+
 		cP = new CommonPage();
+
 		cP204 = new CommonPage_BAU_TC204();
+
+		cPT = new CommonPageTaylor();
+
+		cU = new ContactUsPage();
+
+		case1 = new CommonPage_BAU_TC207();
+
+		cPG = new CommonpageBAU_TC302();
+
+		Cpb = new CommonPage_BAU_TC305();
+
 	}
 }
--- src/main/java/pages/CommonPage.java
+++ src/main/java/pages/CommonPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -13,6 +14,10 @@ public class CommonPage {
 	}
 
 	@FindBy(xpath = "//div[@id='promo-new-session']/button")
+
 	public WebElement closePopup;// Tom
+
+	@FindBy(xpath = "//a[@href='/stores-a-z?hsh=2']") // puthea-Store Menu
+	public WebElement storeMenuElement;
 
 }
