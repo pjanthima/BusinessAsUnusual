215ae60984c6f69f4577e52ea16477a89a181ce1
Janmolina <95003084+Janmolina@users.noreply.github.com>
2022-04-10 00:31:36 -0400

    updated

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -2,11 +2,20 @@ package driver;
 
 import pages.CommonPage;
 
+import pages.CommonPage_BAU_TC206;
+
 public class PageInitializer extends BaseClass {
 
 	public static CommonPage cP;
+	public static CommonPage_BAU_TC206 case1;
 
 	public static void initialize() {
 		cP = new CommonPage();
+		case1 = new CommonPage_BAU_TC206();
+		
+		
+		
+		
 	}
 }
+
---
+++ src/main/java/pages/CommonPage_BAU_TC206.java
@@ -0,0 +1,75 @@
+package pages;
+
+
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import driver.BaseClass;
+
+
+
+
+public class CommonPage_BAU_TC206 {
+  
+     
+
+	
+
+		public CommonPage_BAU_TC206() {
+            PageFactory.initElements(BaseClass.getDriver(), this);
+        }
+        
+       
+        @FindBy(xpath= "//*[@id=\"new-header\"]/section/section[2]/div[2]/section/nav/ul/li[7]/a")
+    	public WebElement  SelectShopLocalButton;
+       
+        @FindBy(id = "//input[@id='near']")
+        public WebElement clearZipCodeBox;
+        
+        @FindBy (xpath = "//input[@id='near']")
+        public WebElement EnterZiPCode;
+        
+        @FindBy(xpath= "//*[@id=\"near\"]")
+    	public WebElement  SelectZipCode;
+        
+        
+        @FindBy(xpath= "//div[@id='shoplocal-searchbar']/form/div[2]/div/button")
+    	public WebElement  SelectSearchButton;
+        
+     
+        
+        @FindBy(xpath= "")
+    	public WebElement  ClickOnFilterButton;
+        
+        //(//h2[contains(text(),'357 Offers')])[1]
+
+
+        @FindBy(xpath= "//*[@id=\"asian\"]")
+    	public WebElement  SelectCategoriesOfRestaurant;
+        
+        @FindBy(xpath= "//*[@id='asian']")
+    	public WebElement  ApplyforSearchRestaurant;
+       
+     
+        @FindBy(xpath= "//div[@class='shoplocal-results__header-center']")
+		private WebElement  searchResult;
+       
+
+    
+		public String searchResult() {
+			
+			return searchResult.getText();
+		}
+
+
+}
+		
+        
+      
+	
+
+
