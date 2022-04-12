1b2c9d8252ee565af8ebb06a83816f31354a72a8
VGomez9o5 <95319026+VGomez9o5@users.noreply.github.com>
2022-04-12 18:52:48 -0400

    Victoria's Code

--- src/main/java/driver/PageInitializer.java
+++ src/main/java/driver/PageInitializer.java
@@ -1,12 +1,20 @@
 package driver;
 
 import pages.CommonPage;
+import pages.CommonPage_BAU_301;
 
 public class PageInitializer extends BaseClass {
 
+	
 	public static CommonPage cP;
+	public static CommonPage_BAU_301 cP301;
 
 	public static void initialize() {
+		
 		cP = new CommonPage();
+		cP301 = new CommonPage_BAU_301();
+		
+
+		
 	}
 }
---
+++ src/main/java/pages/CommonPage_BAU_301.java
@@ -0,0 +1,45 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import driver.BaseClass;
+
+public class CommonPage_BAU_301 {
+
+	public CommonPage_BAU_301() {
+		PageFactory.initElements(BaseClass.getDriver(), this);
+
+	}
+
+	@FindBy(id = "popup2")
+	public WebElement closePopup;
+
+	@FindBy(xpath = "//div[@class='col-lg-6'][2]/div/div[2]/ul/li[1]/a")
+	public WebElement facebookIcon;
+
+	@FindBy(xpath = "//div[@class='col-lg-6'][2]/div/div[2]/ul/li[2]/a")
+	public WebElement instagramIcon;
+
+	@FindBy(xpath = "//div[@class='col-lg-6'][2]/div/div[2]/ul/li[3]/a")
+	public WebElement linkedInIcon;
+
+	@FindBy(xpath = "//div[@class='col-lg-6'][2]/div/div[2]/ul/li[4]/a")
+	public WebElement twitterIcon;
+	
+	@FindBy(xpath = "//a[@aria-label='Facebook']")
+	private WebElement faceBookIcon;
+
+	public boolean verifyFaceBookIcon() {
+		return faceBookIcon.isDisplayed();
+		
+		
+		
+
+		
+		
+		
+	}
+	
+}
